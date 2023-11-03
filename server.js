39ea6cc1a97ee6bcf4ad2da3ac9ac9ea636b5599
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-11-03 21:41:06 +0800

    server created

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const app = require('./app')
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+async function dbConnect() {
+  await mongoose.connect(DB)
+  console.log('DB connection successful')
+}
+
+dbConnect().catch(err=> console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
