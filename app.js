de7e99304937d108b0894286ea194414e7df8666
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-11-05 12:13:33 +0800

    preliminary error handling

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const jobRouter = require('./routes/jobRouter')
+const errorHandler = require('./controllers/errorController')
 
 const app = express()
 app.use(express.json());
 
 app.use('/api/jobs', jobRouter)
+
+app.use(errorHandler)
 
 module.exports = app
