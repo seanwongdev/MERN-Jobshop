b8cd6ca1901fe11c4c07c2661a64f1f1e521c2db
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-11-08 23:24:00 +0800

    adding prod and dev environments to errors

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const jobRouter = require('./routes/jobRouter');
 const errorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRouter');
 
 const app = express();
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+
 
 app.use('/api/jobs', jobRouter);
 app.use('/api/users', userRouter);
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,4 @@
-const AppError = require("../utils/appError");
+const AppError = require('../utils/appError');
 
 const handleJsonWebError = () => new AppError('Invalid token. Please log in again', 401);
 
@@ -11,27 +11,33 @@ const handleValidationError = (err) => {
   return new AppError(message, 400);
 }
 
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+}
 
-const sendError = (err, res) => {
+const sendErrorProd = (err, res) => {
   // Operational, trusted error: sending message to client
-  // if (err.isOperational) {
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      error: err,
       message: err.message,
-      stack: err.stack,
     });
 
-    // Programming or other unknown error: don't leak error details
-  // } else {
-  //   // 1) Log Error
-  //   console.error('Error', err);
+    //Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log Error
+    console.error('Error', err);
 
-  //   // 2) send generic message
-  //   res
-  //     .status(500)
-  //     .json({ status: 'error', message: 'Something went very wrong!' });
-  // }
+    // 2) send generic message
+    res
+      .status(500)
+      .json({ status: 'error', message: 'Something went very wrong!' });
+  }
 };
 
 
@@ -39,12 +45,15 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  let error = Object.create(err);
-  // if (error.name === 'CastError') error = handleCastErrorDB(error);
-  // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-  if (error.name === 'ValidationError') error = handleValidationError(error);
-  if (error.name === 'JsonWebTokenError') error = handleJsonWebError();
-  if (error.name === 'TokenExpiredError') error = handleTokenExpiredError();
-  sendError(error, res);
-
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = Object.create(err);
+    // if (error.name === 'CastError') error = handleCastErrorDB(error);
+    // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError') error = handleValidationError(error);
+    if (error.name === 'JsonWebTokenError') error = handleJsonWebError();
+    if (error.name === 'TokenExpiredError') error = handleTokenExpiredError();
+    sendErrorProd(error, res);
+  }
 };
