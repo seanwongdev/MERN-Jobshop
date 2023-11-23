f536939b820727b98e5298fd554f6b6a73030419
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-11-23 14:56:50 +0800

    added data per month to users patch and admin

--- data/data-job-script.js
+++ data/data-job-script.js
@@ -1,11 +1,12 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const Job = require('../models/jobModel');
+const fs = require("fs");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const Job = require("../models/jobModel");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "../config.env" });
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
 
@@ -14,19 +15,38 @@ dbConnect().catch((err) => console.log(err));
 async function dbConnect() {
   await mongoose.connect(DB);
 
-  console.log('DB connection successful');
+  console.log("DB connection successful");
 }
+
+const jobs = JSON.parse(fs.readFileSync(`${__dirname}/jobs.json`, "utf-8"));
+
+const importData = async () => {
+  try {
+    await Job.create(jobs);
+    console.log("data successfully loaded");
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await mongoose.disconnect();
+  }
+};
 
 const deleteData = async () => {
   try {
     await Job.deleteMany();
-    console.log('data successfully deleted');
+    console.log("data successfully deleted");
     process.exit();
   } catch (err) {
     console.log(err);
+  } finally {
+    await mongoose.disconnect();
   }
 };
 
-if (process.argv[2] === '--delete') {
+if (process.argv[2] === "--import") {
+  importData();
+}
+if (process.argv[2] === "--delete") {
   deleteData();
 }
